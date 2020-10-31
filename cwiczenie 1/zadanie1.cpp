0f91e5f1a9b309c96f9fae7e478ac32569caf6e1
SpaceNuggets <72545029+SpaceNuggets@users.noreply.github.com>
2020-10-31 11:54:35 +0100

    Create zadanie1.cpp

---
+++ cwiczenie 1/zadanie1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+
+using namespace std;
+
+void zamiana(int * a, int * b){
+    if(*b<*a){
+        int nowa=*b;
+        *b=*a;
+        *a=nowa;
+    }
+}
+
+int main()
+{
+  int a,b;
+  cout <<"Podaj liczby"<<endl;
+  cin>>a;
+  cin>>b;
+  cout << "A to "<<a<<" B to "<<b<<endl;
+  zamiana(&a,&b);
+  cout << "Po zamianie A to "<<a<<" B to "<<b<<endl;
+  getch();
+}
